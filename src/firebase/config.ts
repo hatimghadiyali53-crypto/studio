b03db830cb8a4150e5e1a8e0782089e3c118b074
Hatim Ghadiyali <hatimghadiyali53@gmail.com>
2025-10-29 10:09:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9383282358-7c11a",
+  "appId": "1:729323596547:web:191c8b5e0199d237051736",
+  "apiKey": "AIzaSyBesY3TABMBj37aXLvkvww-GXCWsxPsLnU",
+  "authDomain": "studio-9383282358-7c11a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "729323596547"
+};
